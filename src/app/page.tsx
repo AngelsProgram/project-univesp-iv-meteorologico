82ffba2c99948b1ff61eaec3013d6bfafaff6a89
Marcelo Anjos <marcelo164658@edu.marilia.sp.gov.br>
2025-10-01 11:01:11 -0300

    feat(charts): add recharts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home(props: PageProps<"/">) {
-  return "My main page";
+'use client';
+import * as Recharts from 'recharts';
+import { data } from '#/data/dados';
+
+export default function Page(props: PageProps<"/">) {
+  const id_y_axis_left = 'id_y_axis_left';
+  const id_y_axis_right = 'id_y_axis_right';
+
+  return (
+    <Recharts.ResponsiveContainer width='100%' height={500}>
+      <Recharts.ComposedChart data={data}>
+        <Recharts.XAxis dataKey='data' />
+        <Recharts.YAxis yAxisId={id_y_axis_left} orientation='left' />
+        <Recharts.YAxis yAxisId={id_y_axis_right} orientation='right' />
+        <Recharts.Bar dataKey='precipitacao' yAxisId={id_y_axis_left} />
+        <Recharts.Line dataKey='temperatura' yAxisId={id_y_axis_right} />
+        <Recharts.Tooltip />
+        <Recharts.Legend />
+      </Recharts.ComposedChart>
+    </Recharts.ResponsiveContainer>
+  )
 }
---
+++ src/data/dados.ts
@@ -0,0 +1,12 @@
+export const data = [
+    {
+        data: '2025-09-30',
+        precipitacao: 500,
+        temperatura: 24,
+    },
+    {
+        data: '2025-09-29',
+        precipitacao: 400,
+        temperatura: 34,
+    }
+]
